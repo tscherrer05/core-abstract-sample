a999ee956cc79e0b51a42bcf01e847c381c99da3
Timothée Scherrer <timot.scherrer@gmail.com>
2022-04-28 22:27:30 +0200

    Add a new working implementation

--- core-stock/src/main/java/com/example/demo/core/ShoeCoreStock.java
+++ core-stock/src/main/java/com/example/demo/core/ShoeCoreStock.java
@@ -1,4 +1,23 @@
 package com.example.demo.core;
 
-public class ShoeCoreStock {
+import com.example.demo.dto.in.ShoeFilter;
+import com.example.demo.dto.in.ShoeFilter.Color;
+import com.example.demo.dto.out.Shoe;
+import com.example.demo.dto.out.Shoes;
+import java.math.BigInteger;
+import java.util.List;
+
+@Implementation(version = 3)
+public class ShoeCoreStock extends AbstractShoeCore {
+
+    @Override
+    public Shoes search(final ShoeFilter filter) {
+        return Shoes.builder()
+                .shoes(List.of(Shoe.builder()
+                        .name("Stock shoe")
+                        .color(filter.getColor().orElse(Color.BLACK))
+                        .size(filter.getSize().orElse(BigInteger.TWO))
+                        .build()))
+                .build();
+    }
 }
